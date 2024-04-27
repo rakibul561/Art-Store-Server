dad168570f79fb76147cb13a25381fcb6b4ce203
Rakib <rakibulhasan3929@gmail.com>
2024-04-28 01:21:57 +0600

    user added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 // midaleware
 app.use(cors());
 app.use(express.json());
-
+// 
 // artMaster
 // 1M5pvv90f23lATEr
 console.log(process.env.DB_USER);
@@ -30,6 +30,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const artCollection = client.db('artDB').collection('art')
+
+    app.get('/art',async (req, res )=>{
+         const cursor = artCollection.find();
+         const result = await cursor.toArray();
+         res.send(result);
+    })
+
+
+   app.post('/art',async(req,res)=>{
+     const newArt  = req.body;
+     console.log(newArt);
+     const result = await artCollection.insertOne(newArt)
+     res.send(result);
+   })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
