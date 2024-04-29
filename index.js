3055a802e288cf93940a976ccd72be887e1e3073
Rakib <rakibulhasan3929@gmail.com>
2024-04-29 22:15:10 +0600

    added update

--- index.js
+++ index.js
@@ -33,43 +33,75 @@ async function run() {
 
     const artCollection = client.db('artDB').collection('art')
 
-    app.get('/art',async (req, res )=>{
-         const cursor = artCollection.find();
-         const result = await cursor.toArray();
-         res.send(result);
+    app.get('/art', async (req, res) => {
+      const cursor = artCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
-    app.get('/newArt/:id',async (req, res )=>{
+    app.get('/newArt/:id', async (req, res) => {
       const result = await artCollection.findOne({
-        _id: new ObjectId (req.params.id),
+        _id: new ObjectId(req.params.id),
       });
       res.send(result)
     })
 
-   app.get("/myArt/:email", async (req,res)=>{
-    console.log( req.params.email);
-    const result = await artCollection.find({
-      email: req.params.email}).toArray();
-    res.send(result)
-   })
+    app.get("/myArt/:email", async (req, res) => {
+      console.log(req.params.email);
+      const result = await artCollection.find({
+        email: req.params.email
+      }).toArray();
+      res.send(result)
+    })
+
+    app.get('/art/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await artCollection.findOne(query);
+      res.send(result);
+    })
 
 
-  //  
+    app.post('/art', async (req, res) => {
+      const newArt = req.body;
+      console.log(newArt);
+      const result = await artCollection.insertOne(newArt)
+      res.send(result);
+    })
+
+    app.put('/art/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updatedItem = req.body;
+      const coffee = {
+
+        $set: {
+          name: updatedItem.name,
+          subcategory_Name: updatedItem.subcategory_Name,
+          short_description: updatedItem.short_description,
+          photo: updatedItem.photo,
+          price: updatedItem.price,
+          rating: updatedItem.rating,
+          processing_time: updatedItem.processing_time,
+          stockStatus: updatedItem.stockStatus,
+          customization: updatedItem.customization
+
+        }
+
+      }
+      const result = await artCollection.updateOne(filter, coffee, options);
+      res.send(result)
+    })
 
 
-   app.post('/art',async(req,res)=>{
-     const newArt  = req.body;
-     console.log(newArt);
-     const result = await artCollection.insertOne(newArt)
-     res.send(result);
-   })
 
-   app.delete('/art/:id', async(req, res)=>{
-     const id = req.params.id;
-     const query = {_id: new ObjectId(id)}
-     const result = await artCollection.deleteOne(query)
-     res.send(result);
-   })
+    app.delete('/art/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await artCollection.deleteOne(query)
+      res.send(result);
+    })
 
 
 
@@ -86,12 +118,12 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
-    res.send('server is running')
+app.get('/', (req, res) => {
+  res.send('server is running')
 })
 
-app.listen(port,()=>{
-    console.log(`art server is running${port}`);
+app.listen(port, () => {
+  console.log(`art server is running${port}`);
 })
 
 
